6e550137b470783a95a695e3930aff6292802e19
Kenta <102387086+KentaKK@users.noreply.github.com>
2023-09-01 19:21:41 +0200

    Update common.lua

--- server/common.lua
+++ server/common.lua
@@ -1,84 +1,83 @@
 ESX = {}
 ESX.Players = {}
-ESX.UsableItemsCallbacks = {}
-ESX.Items = {}
-ESX.ServerCallbacks = {}
-ESX.TimeoutCount = -1
-ESX.CancelledTimeouts = {}
-ESX.Pickups = {}
-ESX.PickupId = 0
 ESX.Jobs = {}
-ESX.RegisteredCommands = {}
+ESX.Items = {}
+Core = {}
+Core.UsableItemsCallbacks = {}
+Core.RegisteredCommands = {}
+Core.Pickups = {}
+Core.PickupId = 0
+Core.PlayerFunctionOverrides = {}
+Core.DatabaseConnected = false
+Core.playersByIdentifier = {}
 
-AddEventHandler('esx:getSharedObject', function(cb)
-	cb(ESX)
+Core.vehicleTypesByModel = {}
+
+exports('getSharedObject', function()
+  return ESX
 end)
 
-function getSharedObject()
-	return ESX
+AddEventHandler("esx:getSharedObject", function()
+	local Invoke = GetInvokingResource()
+	print(("[^1ERROR^7] Resource ^5%s^7 Used the ^5getSharedObject^7 Event, this event ^1no longer exists!^7 Visit https://documentation.esx-framework.org/tutorials/sharedevent for how to fix!"):format(Invoke))
+end)
+
+if GetResourceState('ox_inventory') ~= 'missing' then
+  Config.OxInventory = true
+  Config.PlayerFunctionOverride = 'OxInventory'
+  SetConvarReplicated('inventory:framework', 'esx')
+  SetConvarReplicated('inventory:weight', Config.MaxWeight * 1000)
 end
 
 local function StartDBSync()
-	Citizen.CreateThread(function()
-		Citizen.Wait(10 * 60 * 1000)
-		ESX.SavePlayers()
-	end)
+  CreateThread(function()
+    while true do
+      Wait(40 * 60 * 1000)
+      Core.SavePlayers()
+    end
+  end)
 end
 
 MySQL.ready(function()
-	MySQL.Async.fetchAll('SELECT * FROM items', {}, function(result)
-		for k,v in ipairs(result) do
-			ESX.Items[v.name] = {
-				label = v.label,
-				weight = v.weight,
-				rare = v.rare,
-				canRemove = v.can_remove
-			}
-		end
-	end)
+  if not Config.OxInventory then
+    local items = MySQL.query.await('SELECT * FROM items')
+    for k, v in ipairs(items) do
+      ESX.Items[v.name] = {label = v.label, weight = v.weight, rare = v.rare, canRemove = v.can_remove}
+    end
+  else
+    TriggerEvent('__cfx_export_ox_inventory_Items', function(ref)
+      if ref then
+        ESX.Items = ref()
+      end
+    end)
 
-	local Jobs = {}
-	MySQL.Async.fetchAll('SELECT * FROM jobs', {}, function(jobs)
-		for k,v in ipairs(jobs) do
-			Jobs[v.name] = v
-			Jobs[v.name].grades = {}
-		end
+    AddEventHandler('ox_inventory:itemList', function(items)
+      ESX.Items = items
+    end)
 
-		MySQL.Async.fetchAll('SELECT * FROM job_grades', {}, function(jobGrades)
-			for k,v in ipairs(jobGrades) do
-				if Jobs[v.job_name] then
-					Jobs[v.job_name].grades[tostring(v.grade)] = v
-				else
-					print(('[^3WARNING^7] Ignoring job grades for ^5"%s"^0 due to missing job'):format(v.job_name))
-				end
-			end
+    while not next(ESX.Items) do
+      Wait(0)
+    end
+  end
 
-			for k2,v2 in pairs(Jobs) do
-				if ESX.Table.SizeOf(v2.grades) == 0 then
-					Jobs[v2.name] = nil
-					print(('[^3WARNING^7] Ignoring job ^5"%s"^0due to no job grades found'):format(v2.name))
-				end
-			end
-			ESX.Jobs = Jobs
-			print('[^2INFO^7] ESX ^5Legacy^0 initialized')
-			StartDBSync()
-			StartPayCheck()
-		end)
-	end)
+  ESX.RefreshJobs()
+
+  print(('[^2INFO^7] ESX ^5Legacy %s^0 initialized!'):format(GetResourceMetadata(GetCurrentResourceName(), "version", 0)))
+
+  StartDBSync()
+  StartPayCheck()
 end)
 
 RegisterServerEvent('esx:clientLog')
 AddEventHandler('esx:clientLog', function(msg)
-	if Config.EnableDebug then
-		print(('[^2TRACE^7] %s^7'):format(msg))
-	end
+  if Config.EnableDebug then
+    print(('[^2TRACE^7] %s^7'):format(msg))
+  end
 end)
 
-RegisterServerEvent('esx:triggerServerCallback')
-AddEventHandler('esx:triggerServerCallback', function(name, requestId, ...)
-	local playerId = source
-
-	ESX.TriggerServerCallback(name, requestId, playerId, function(...)
-		TriggerClientEvent('esx:serverCallback', playerId, requestId, ...)
-	end, ...)
+RegisterNetEvent("esx:ReturnVehicleType", function(Type, Request)
+  if Core.ClientCallbacks[Request] then
+    Core.ClientCallbacks[Request](Type)
+    Core.ClientCallbacks[Request] = nil
+  end
 end)
