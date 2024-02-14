67caaa329188846bc555766562d85b7d570f9b4b
Kenta <102387086+KentaKK@users.noreply.github.com>
2024-02-14 21:26:18 +0100

    More accurate

--- client/deformation.lua
+++ client/deformation.lua
@@ -1,358 +1,357 @@
-local MAX_DEFORM_ITERATIONS = 82
-local DEFORMATION_DAMAGE_THRESHOLD = 0.05
+local MAX_DEFORM_ITERATIONS = 150
+local DEFORMATION_DAMAGE_THRESHOLD = 0.003
 
 exports("GetVehicleDeformation", GetVehicleDeformation)
 exports("SetVehicleDeformation", SetVehicleDeformation)
 
----@param vehicle number|string
+---This might need some more experimentation
+---@param vehicle number
 ---@param deformationPoints table
----@param callback function?
-SetVehicleDeformation = function(vehicle, deformationPoints, callback)
-    --assert(vehicle ~= nil and DoesEntityExist(vehicle), "Parameter \"vehicle\" must be a valid vehicle entity!")
-    --assert(deformationPoints ~= nil and type(deformationPoints) == "table", "Parameter \"deformationPoints\" must be a table!")
+---@param cb function?
+SetVehicleDeformation = function(vehicle, deformationPoints, cb)
     CreateThread(function()
-	local min, max = GetModelDimensions(GetEntityModel(vehicle))
-        local damageMult = #(max - min) * 3.5	-- might need some more experimentation
-
-        local printMsg = false
-		for i, def in ipairs(deformationPoints) do
-			def[1] = vector3(Round(def[1].x, 2), Round(def[1].y, 2), Round(def[1].z, 2))
+	    local min, max = GetModelDimensions(GetEntityModel(vehicle))
+		local fDeformationDamageMult = GetVehicleHandlingFloat(vehicle, "CHandlingData", "fDeformationDamageMult")
+		local damageMult = #(max - min)
+		if Config.EnableDebug then
+			print(string.format("First deform damagemult: %s", damageMult))
 		end
+		if (fDeformationDamageMult <= 0.55) then
+			damageMult = #(max - min) * 12.0
+		elseif (fDeformationDamageMult <= 0.65) then
+			damageMult = #(max - min) * 10.0
+		elseif (fDeformationDamageMult <= 0.9) then
+			damageMult = #(max - min) * 2.0
+		end
+		if Config.EnableDebug then
+			print(string.format("Deform damagemult set: %s", damageMult))
+		end
+
 		local deform = true
 		local iteration = 0
+		local d = 0
 		while deform and iteration < MAX_DEFORM_ITERATIONS do
 			deform = false
-			for i, def in ipairs(deformationPoints) do
-				if #GetVehicleDeformationAtPos(vehicle, def[1].x, def[1].y, def[1].z) < Round(def[2]*0.99,2) then
-					SetVehicleDamage(vehicle, def[1].x * 2.0, def[1].y * 2.0, def[1].z * 2.0, Round(def[2] * damageMult, 2), 1000.0, true)
+			for _, def in ipairs(deformationPoints) do
+				if #GetVehicleDeformationAtPos(vehicle, def[1].x, def[1].y, def[1].z) < def[2] then
+					SetVehicleDamage(vehicle, def[1].x, def[1].y, def[1].z, def[2] * damageMult, 2000.0, true)
 					deform = true
+					d += 1
 				end
 			end
 			iteration += 1
-			Wait(100)
+			Wait(10)
 		end
-        if (callback) then
-	        callback()
+		if Config.EnableDebug then
+			print(string.format("Deforms set: %s", d))
+		end
+        if cb then
+	        cb()
         end
     end)
 end
 
----@param vehicle number|string
+---@param vehicle number
 ---@return table
 GetVehicleDeformation = function(vehicle)
-    assert(vehicle ~= nil and DoesEntityExist(vehicle), "Parameter \"vehicle\" must be a valid vehicle entity!")
 	local min, max = GetModelDimensions(GetEntityModel(vehicle))
-	local X = Round((max.x - min.x) * 0.5, 2)
-	local Y = Round((max.y - min.y) * 0.5, 2)
-	local Z = Round((max.z - min.z) * 0.5, 2)
-	local halfY = Round(Y * 0.5, 2)
+	local X = (max.x - min.x)
+	local Y = (max.y - min.y)
+	local Z = (max.z - min.z)
+	local halfY = Y
+
 	local positions = {
 		vector3(-X, Y,  0.0),
 		vector3(-X, Y,  Z),
-
 		vector3(0.0, Y,  0.0),
 		vector3(0.0, Y,  Z),
-
 		vector3(X, Y,  0.0),
 		vector3(X, Y,  Z),
-
 		vector3(-X, halfY,  0.0),
 		vector3(-X, halfY,  Z),
-                --új
 	    vector3(-X, halfY + (halfY / 2),  0.0),
 		vector3(-X, halfY + (halfY / 2),  Z),
-
 		vector3(-X, halfY - (halfY / 2),  0.0),
 		vector3(-X, halfY - (halfY / 2),  Z),
-                --
 		vector3(0.0, halfY,  0.0),
 		vector3(0.0, halfY,  Z),
-
 		vector3(X, halfY,  0.0),
 		vector3(X, halfY,  Z),
-                --új
 	    vector3(X, halfY + (halfY / 2),  0.0),
 		vector3(X, halfY + (halfY / 2),  Z),
-
 		vector3(X, halfY - (halfY / 2),  0.0),
 		vector3(X, halfY - (halfY / 2),  Z),
-                --
 		vector3(-X, 0.0,  0.0),
 		vector3(-X, 0.0,  Z),
-
 		vector3(0.0, 0.0,  0.0),
 		vector3(0.0, 0.0,  Z),
-
 		vector3(X, 0.0,  0.0),
 		vector3(X, 0.0,  Z),
-
 		vector3(-X, -halfY,  0.0),
 		vector3(-X, -halfY,  Z),
-
 	    vector3(-X, -(halfY + (halfY / 2)),  0.0),
 		vector3(-X, -(halfY + (halfY / 2)),  Z),
-
 		vector3(-X, -(halfY - (halfY / 2)),  0.0),
 		vector3(-X, -(halfY - (halfY / 2)),  Z),
-
 		vector3(0.0, -halfY,  0.0),
 		vector3(0.0, -halfY,  Z),
-
 		vector3(0.0, -(halfY + (halfY / 2)),  0.0),
 		vector3(0.0, -(halfY + (halfY / 2)),  Z),
-
 		vector3(0.0, -(halfY - (halfY / 2)),  0.0),
 		vector3(0.0, -(halfY - (halfY / 2)),  Z),
-
 		vector3(X, -halfY,  0.0),
 		vector3(X, -halfY,  Z),
-
 		vector3(X, -(halfY + (halfY / 2)),  0.0),
 		vector3(X, -(halfY + (halfY / 2)),  Z),
-
 		vector3(X, -(halfY - (halfY / 2)),  0.0),
 		vector3(X, -(halfY - (halfY / 2)),  Z),
-
 		vector3(-X, -Y,  0.0),
 		vector3(-X, -Y,  Z),
-
 		vector3(0.0, -Y,  0.0),
 		vector3(0.0, -Y,  Z),
-
 		vector3(X, -Y,  0.0),
 		vector3(X, -Y,  Z),
-
 		vector3(-(X / 2), Y,  0.0),
 		vector3(-(X / 2), Y,  Z),
-
 		vector3((X / 2), Y,  0.0),
 		vector3((X / 2), Y,  Z),
-
 		vector3(-(X / 2), halfY,  0.0),
 		vector3(-(X / 2), halfY,  Z),
-
 		vector3(-(X / 2), halfY + (halfY / 2),  0.0),
 		vector3(-(X / 2), halfY + (halfY / 2),  Z),
-
 		vector3(-(X / 2), halfY - (halfY / 2),  0.0),
 		vector3(-(X / 2), halfY - (halfY / 2),  Z),
 		vector3((X / 2), halfY + (halfY / 2),  0.0),
 		vector3((X / 2), halfY + (halfY / 2),  Z),
-
 		vector3((X / 2), halfY - (halfY / 2),  0.0),
 		vector3((X / 2), halfY - (halfY / 2),  Z),
-
 		vector3(-(X / 2), 0.0,  0.0),
 		vector3(-(X / 2), 0.0,  Z),
-
 		vector3(-(X / 2), -halfY,  0.0),
 		vector3(-(X / 2), -halfY,  Z),
-
 		vector3(-(X / 2), -(halfY + (halfY / 2)),  0.0),
 		vector3(-(X / 2), -(halfY + (halfY / 2)),  Z),
-
 		vector3(-(X / 2), -(halfY - (halfY / 2)),  0.0),
 		vector3(-(X / 2), -(halfY - (halfY / 2)),  Z),
-
 		vector3((X / 2), -halfY,  0.0),
 		vector3((X / 2), -halfY,  Z),
-
 		vector3((X / 2), -(halfY + (halfY / 2)),  0.0),
 		vector3((X / 2), -(halfY + (halfY / 2)),  Z),
-
 		vector3((X / 2), -(halfY - (halfY / 2)),  0.0),
 		vector3((X / 2), -(halfY - (halfY / 2)),  Z),
-
 		vector3(-(X / 2), -Y,  0.0),
 		vector3(-(X / 2), -Y,  Z),
-
 		vector3((X / 2), -Y,  0.0),
 		vector3((X / 2), -Y,  Z),
 		vector3(-X, Y,  -Z),
-
 		vector3(0.0, Y,  -Z),
-
 		vector3(X, Y,  -Z),
-
 		vector3(-X, halfY,  -Z),
-
 		vector3(-X, halfY + (halfY / 2),  -Z),
-
 		vector3(-X, halfY - (halfY / 2),  -Z),
-
 		vector3(0.0, halfY,  -Z),
-
 		vector3(X, halfY + (halfY / 2),  -Z),
-
 		vector3(X, halfY - (halfY / 2),  -Z),
-
 		vector3(-X, 0.0,  -Z),
-
 		vector3(0.0, 0.0,  -Z),
-
 		vector3(X, 0.0,  -Z),
-
 		vector3(-X, -halfY,  -Z),
-
 		vector3(-X, -(halfY + (halfY / 2)),  -Z),
-
 		vector3(-X, -(halfY - (halfY / 2)),  -Z),
-
 		vector3(0.0, -halfY,  -Z),
-
 		vector3(0.0, -(halfY + (halfY / 2)),  -Z),
-
 		vector3(0.0, -(halfY - (halfY / 2)),  -Z),
-
 		vector3(X, -halfY,  -Z),
-
 		vector3(X, -(halfY + (halfY / 2)),  -Z),
-
 		vector3(X, -(halfY - (halfY / 2)),  -Z),
-
 		vector3(-X, -Y,  -Z),
-
 		vector3(0.0, -Y,  -Z),
-
 		vector3(X, -Y,  -Z),
-
 		vector3(-(X / 2), Y,  -Z),
-
 		vector3((X / 2), Y,  -Z),
-
 		vector3(-(X / 2), halfY,  -Z),
-
 		vector3(-(X / 2), halfY + (halfY / 2),  -Z),
-
 		vector3(-(X / 2), halfY - (halfY / 2),  -Z),
 		vector3((X / 2), halfY + (halfY / 2),  -Z),
-
 		vector3((X / 2), halfY - (halfY / 2),  -Z),
-
 		vector3(-(X / 2), 0.0,  -Z),
-
 		vector3(-(X / 2), -halfY,  -Z),
-
 		vector3(-(X / 2), -(halfY + (halfY / 2)),  -Z),
-
 		vector3(-(X / 2), -(halfY - (halfY / 2)),  -Z),
-
 		vector3((X / 2), -halfY,  -Z),
-
 		vector3((X / 2), -(halfY + (halfY / 2)),  -Z),
-
 		vector3((X / 2), -(halfY - (halfY / 2)),  -Z),
-
-
 		vector3(-(X / 2), -Y,  -Z),
-
 		vector3((X / 2), -Y,  -Z),
 	}
-        local deformationPoints = {}
-	for i, pos in ipairs(positions) do
-		local dmg = math.floor(#(GetVehicleDeformationAtPos(vehicle, pos.x, pos.y, pos.z)) * 1000.0) / 1000.0
+
+    local deformationPoints = {}
+
+	for i=1, #positions do
+		local pos = positions[i]
+		local dmg = #GetVehicleDeformationAtPos(vehicle, pos.x, pos.y, pos.z)
 		if dmg > DEFORMATION_DAMAGE_THRESHOLD then
-			table.insert(deformationPoints, { pos, dmg })
+			if Config.EnableDebug then
+				print(dmg)
+			end
+			deformationPoints[#deformationPoints + 1] = {pos, dmg}
 		end
+	end
+	if Config.EnableDebug then
+		print(string.format("Deforms got: %s", #deformationPoints))
 	end
 	return deformationPoints
 end
 
----@param newDef table
----@param oldDef table
----@return boolean
-IsDeformationWorse = function(newDef, oldDef)
-  if newDef == nil and oldDef == nil then return false end
-	if (oldDef == nil or #newDef > #oldDef) then
-		return true
-	elseif (#newDef < #oldDef) then
-		return false
-	end
-
-	for i, new in ipairs(newDef) do
-		local found = false
-		for j, old in ipairs(oldDef) do
-			if (new[1] == old[1]) then
-				found = true
-
-				if (new[2] > old[2]) then
-					return true
-				end
+if Config.EnableDebug then
+	CreateThread(function()
+		while true do
+		local sleep = 1000
+		local ped = PlayerPedId()
+		if IsPedInAnyVehicle(ped, true) then
+			sleep = 0
+			local vehicle = GetVehiclePedIsIn(ped, false)
+			local min, max = GetModelDimensions(GetEntityModel(vehicle))
+			local X = (max.x - min.x)
+			local Y = (max.y - min.y)
+			local Z = (max.z - min.z)
+			local halfY = Y
+			local positions = {
+				vector3(-X, Y,  0.0),
+				vector3(-X, Y,  Z),
+				vector3(0.0, Y,  0.0),
+				vector3(0.0, Y,  Z),
+				vector3(X, Y,  0.0),
+				vector3(X, Y,  Z),
+				vector3(-X, halfY,  0.0),
+				vector3(-X, halfY,  Z),
+				vector3(-X, halfY + (halfY / 2),  0.0),
+				vector3(-X, halfY + (halfY / 2),  Z),
+				vector3(-X, halfY - (halfY / 2),  0.0),
+				vector3(-X, halfY - (halfY / 2),  Z),
+				vector3(0.0, halfY,  0.0),
+				vector3(0.0, halfY,  Z),
+				vector3(X, halfY,  0.0),
+				vector3(X, halfY,  Z),
+				vector3(X, halfY + (halfY / 2),  0.0),
+				vector3(X, halfY + (halfY / 2),  Z),
+				vector3(X, halfY - (halfY / 2),  0.0),
+				vector3(X, halfY - (halfY / 2),  Z),
+				vector3(-X, 0.0,  0.0),
+				vector3(-X, 0.0,  Z),
+				vector3(0.0, 0.0,  0.0),
+				vector3(0.0, 0.0,  Z),
+				vector3(X, 0.0,  0.0),
+				vector3(X, 0.0,  Z),
+				vector3(-X, -halfY,  0.0),
+				vector3(-X, -halfY,  Z),
+				vector3(-X, -(halfY + (halfY / 2)),  0.0),
+				vector3(-X, -(halfY + (halfY / 2)),  Z),
+				vector3(-X, -(halfY - (halfY / 2)),  0.0),
+				vector3(-X, -(halfY - (halfY / 2)),  Z),
+				vector3(0.0, -halfY,  0.0),
+				vector3(0.0, -halfY,  Z),
+				vector3(0.0, -(halfY + (halfY / 2)),  0.0),
+				vector3(0.0, -(halfY + (halfY / 2)),  Z),
+				vector3(0.0, -(halfY - (halfY / 2)),  0.0),
+				vector3(0.0, -(halfY - (halfY / 2)),  Z),
+				vector3(X, -halfY,  0.0),
+				vector3(X, -halfY,  Z),
+				vector3(X, -(halfY + (halfY / 2)),  0.0),
+				vector3(X, -(halfY + (halfY / 2)),  Z),
+				vector3(X, -(halfY - (halfY / 2)),  0.0),
+				vector3(X, -(halfY - (halfY / 2)),  Z),
+				vector3(-X, -Y,  0.0),
+				vector3(-X, -Y,  Z),
+				vector3(0.0, -Y,  0.0),
+				vector3(0.0, -Y,  Z),
+				vector3(X, -Y,  0.0),
+				vector3(X, -Y,  Z),
+				vector3(-(X / 2), Y,  0.0),
+				vector3(-(X / 2), Y,  Z),
+				vector3((X / 2), Y,  0.0),
+				vector3((X / 2), Y,  Z),
+				vector3(-(X / 2), halfY,  0.0),
+				vector3(-(X / 2), halfY,  Z),
+				vector3(-(X / 2), halfY + (halfY / 2),  0.0),
+				vector3(-(X / 2), halfY + (halfY / 2),  Z),
+				vector3(-(X / 2), halfY - (halfY / 2),  0.0),
+				vector3(-(X / 2), halfY - (halfY / 2),  Z),
+				vector3((X / 2), halfY + (halfY / 2),  0.0),
+				vector3((X / 2), halfY + (halfY / 2),  Z),
+				vector3((X / 2), halfY - (halfY / 2),  0.0),
+				vector3((X / 2), halfY - (halfY / 2),  Z),
+				vector3(-(X / 2), 0.0,  0.0),
+				vector3(-(X / 2), 0.0,  Z),
+				vector3(-(X / 2), -halfY,  0.0),
+				vector3(-(X / 2), -halfY,  Z),
+				vector3(-(X / 2), -(halfY + (halfY / 2)),  0.0),
+				vector3(-(X / 2), -(halfY + (halfY / 2)),  Z),
+				vector3(-(X / 2), -(halfY - (halfY / 2)),  0.0),
+				vector3(-(X / 2), -(halfY - (halfY / 2)),  Z),
+				vector3((X / 2), -halfY,  0.0),
+				vector3((X / 2), -halfY,  Z),
+				vector3((X / 2), -(halfY + (halfY / 2)),  0.0),
+				vector3((X / 2), -(halfY + (halfY / 2)),  Z),
+				vector3((X / 2), -(halfY - (halfY / 2)),  0.0),
+				vector3((X / 2), -(halfY - (halfY / 2)),  Z),
+				vector3(-(X / 2), -Y,  0.0),
+				vector3(-(X / 2), -Y,  Z),
+				vector3((X / 2), -Y,  0.0),
+				vector3((X / 2), -Y,  Z),
+				vector3(-X, Y,  -Z),
+				vector3(0.0, Y,  -Z),
+				vector3(X, Y,  -Z),
+				vector3(-X, halfY,  -Z),
+				vector3(-X, halfY + (halfY / 2), -Z),
+				vector3(-X, halfY - (halfY / 2), -Z),
+				vector3(0.0, halfY, -Z),
+				vector3(X, halfY + (halfY / 2), -Z),
+				vector3(X, halfY - (halfY / 2), -Z),
+				vector3(-X, 0.0, -Z),
+				vector3(0.0, 0.0, -Z),
+				vector3(X, 0.0, -Z),
+				vector3(-X, -halfY, -Z),
+				vector3(-X, -(halfY + (halfY / 2)), -Z),
+				vector3(-X, -(halfY - (halfY / 2)), -Z),
+				vector3(0.0, -halfY, -Z),
+				vector3(0.0, -(halfY + (halfY / 2)), -Z),
+				vector3(0.0, -(halfY - (halfY / 2)), -Z),
+				vector3(X, -halfY, -Z),
+				vector3(X, -(halfY + (halfY / 2)), -Z),
+				vector3(X, -(halfY - (halfY / 2)), -Z),
+				vector3(-X, -Y, -Z),
+				vector3(0.0, -Y, -Z),
+				vector3(X, -Y, -Z),
+				vector3(-(X / 2), Y, -Z),
+				vector3((X / 2), Y, -Z),
+				vector3(-(X / 2), halfY,  -Z),
+				vector3(-(X / 2), halfY + (halfY / 2), -Z),
+				vector3(-(X / 2), halfY - (halfY / 2), -Z),
+				vector3((X / 2), halfY + (halfY / 2), -Z),
+				vector3((X / 2), halfY - (halfY / 2), -Z),
+				vector3(-(X / 2), 0.0, -Z),
+				vector3(-(X / 2), -halfY, -Z),
+				vector3(-(X / 2), -(halfY + (halfY / 2)), -Z),
+				vector3(-(X / 2), -(halfY - (halfY / 2)), -Z),
+				vector3((X / 2), -halfY, -Z),
+				vector3((X / 2), -(halfY + (halfY / 2)), -Z),
+				vector3((X / 2), -(halfY - (halfY / 2)), -Z),
+				vector3(-(X / 2), -Y, -Z),
+				vector3((X / 2), -Y, -Z),
+			}
+			for i=1, #positions do
+				local c = positions[i]
+				DrawMarker(28, c.x, c.y, c.z, c.x, c.y, c.z, c.x, c.y, c.z,
+				c.x, c.y, c.z, 14, 230, 14,
+				100, false, true, 2, false, false, false, false)
 			end
+			local c2 = GetEntityCoords(ped)
+			DrawMarker(28, c2.x, c2.y, c2.z + 2, 0.0, 0.0, 0.0, 0.0, 0.0, 0.2,
+			0.2, 0.2, 0.2, 14, 230, 14,
+			100, false, true, 2, false, false, false, false)
 		end
-
-		if (not found) then
-			return true
-		end
-	end
-
-	return false
-end
-
----@param vehicle number|string
-GetVehicleOffsetsForDeformation = function(vehicle)
-	local min, max = GetModelDimensions(GetEntityModel(vehicle))
-	local X = Round((max.x - min.x) * 0.5, 2)
-	local Y = Round((max.y - min.y) * 0.5, 2)
-	local Z = Round((max.z - min.z) * 0.5, 2)
-	local halfY = Round(Y * 0.5, 2)
-
-	return {
-		vector3(-X, Y,  0.0),
-		vector3(-X, Y,  Z),
-
-		vector3(0.0, Y,  0.0),
-		vector3(0.0, Y,  Z),
-
-		vector3(X, Y,  0.0),
-		vector3(X, Y,  Z),
-
-
-		vector3(-X, halfY,  0.0),
-		vector3(-X, halfY,  Z),
-
-		vector3(0.0, halfY,  0.0),
-		vector3(0.0, halfY,  Z),
-
-		vector3(X, halfY,  0.0),
-		vector3(X, halfY,  Z),
-
-
-		vector3(-X, 0.0,  0.0),
-		vector3(-X, 0.0,  Z),
-
-		vector3(0.0, 0.0,  0.0),
-		vector3(0.0, 0.0,  Z),
-
-		vector3(X, 0.0,  0.0),
-		vector3(X, 0.0,  Z),
-
-
-		vector3(-X, -halfY,  0.0),
-		vector3(-X, -halfY,  Z),
-
-		vector3(0.0, -halfY,  0.0),
-		vector3(0.0, -halfY,  Z),
-
-		vector3(X, -halfY,  0.0),
-		vector3(X, -halfY,  Z),
-
-
-		vector3(-X, -Y,  0.0),
-		vector3(-X, -Y,  Z),
-
-		vector3(0.0, -Y,  0.0),
-		vector3(0.0, -Y,  Z),
-
-		vector3(X, -Y,  0.0),
-		vector3(X, -Y,  Z),
-	}
-end
-
-function Round(value, numDecimals)
-	return math.floor(value * 10^numDecimals) / 10^numDecimals
-end
-
-function toPercent(v)
-  return math.floor(v * 100) / 1000
+		Wait(sleep)
+	    end
+	end)
 end
